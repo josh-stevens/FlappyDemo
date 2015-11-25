7218946b9b6eb2cc46a7b838a130a545041e75ed
josh-stevens <jcstevens12@outlook.com>
2015-11-25 16:07:01 -0600

    Basic libgdx project generated

---
+++ core/src/net/joshstevens/flappy/States/State.java
@@ -0,0 +1,24 @@
+package net.joshstevens.flappy.States;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
+
+/**
+ * Created by joshstevens on 11/25/15.
+ */
+public abstract class State {
+    protected OrthographicCamera cam;
+    protected Vector3 mouse;
+    protected GameStateManager gsm;
+
+    protected State(GameStateManager gsm) {
+        this.gsm = gsm;
+        cam = new OrthographicCamera();
+        mouse = new Vector3();
+    }
+
+    protected abstract void handleInput();
+    public abstract void update(float dt);
+    public abstract void render(SpriteBatch sb);
+}
---
+++ desktop/src/net/joshstevens/flappy/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package net.joshstevens.flappy.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.joshstevens.flappy.FlappyDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = FlappyDemo.WIDTH;
+		config.height = FlappyDemo.HEIGHT;
+		config.title = FlappyDemo.TITLE;
+		new LwjglApplication(new FlappyDemo(), config);
+	}
+}
